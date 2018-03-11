045febacea6895ec0be38271281aa1a0bbe2a455
Garadholg <click52100@gmail.com>
2018-03-11 17:15:21 +0100

    Added task 1

---
+++ Data Structure and Algorithms/Tasks/Task 1/ComplexNumber.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+
+class ComplexNumber
+{
+public:
+	ComplexNumber();
+	ComplexNumber(int z, int i);
+	ComplexNumber(ComplexNumber k1, ComplexNumber k2);
+	void set_real(int x);
+	void set_imaginary(int x);
+	std::string get();
+private:
+	int real;
+	int imaginary;
+};
